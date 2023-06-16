c6e46541a632bd289f92076757ee27527f06b324
karthicbz <pkarthicbz@live.in>
2023-06-16 10:18:45 +0530

    create files for models

---
+++ models/category.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema;
+
+const CategorySchema = new mongoose.Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+  childs: [{ type: mongoose.Schema.Types.ObjectId, ref: "ChildCategories" }],
+});
+
+module.exports = mongoose.model("Category", CategorySchema);
