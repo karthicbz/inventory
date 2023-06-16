72f92d2b6bbde9e2e29bd776205c7637ff95d524
Karthick <pkarthicbz@live.in>
2023-06-16 14:02:53 +0530

    create categoryController

---
+++ controllers/categoryController.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const asynchandler = require("express-async-handler");
+
+exports.category_list = asynchandler((req, res, next) => {});
+
+exports.category_create_get = asynchandler((req, res, next) => {});
+
+exports.category_create_post = asynchandler((req, res, next) => {});
+
+exports.category_delete_get = asynchandler((req, res, next) => {});
+
+exports.category_delete_post = asynchandler((req, res, next) => {});
+
+exports.category_update_get = asynchandler((req, res, next) => {});
+
+exports.category_update_post = asynchandler((req, res, next) => {});
