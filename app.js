a323188c115a827834ea084e5fbc976a2bc1ba7e
karthicbz <pkarthicbz@live.in>
2023-06-19 09:39:27 +0530

    add compression module

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
+const compression = require('compression');
 
 var app = express();
 // let categories = [];
@@ -19,37 +20,8 @@ const mongoDB =
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
-  // await addNewCategory();
-  // await addChildCategory();
 }
 
-// async function createCategory(name) {
-//   const category = new Category({ name: name });
-//   await category.save();
-//   categories.push(category);
-//   console.log("Category added: " + name);
-// }
-
-// async function createChildCategory(name, category) {
-//   const childCategory = new ChildCategory({ name: name, category: category });
-//   await childCategory.save();
-//   console.log("child category added: " + name);
-// }
-
-// async function addNewCategory() {
-//   Promise.all([
-//     await createCategory("aluminium"),
-//     await createCategory("copper"),
-//   ]);
-// }
-
-// async function addChildCategory() {
-//   if (categories.length > 0) {
-//     Promise.all([await createChildCategory("Aluminium Rod", categories[0])]);
-//   } else {
-//     console.log("Create categories first");
-//   }
-// }
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -58,6 +30,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
