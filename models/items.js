dd9b84ff75500a29b3cebc05aea09dfdec3c15a9
karthicbz <pkarthicbz@live.in>
2023-07-06 10:49:27 +0530

    add input field for hsn code

--- models/items.js
+++ models/items.js
@@ -5,6 +5,7 @@ const ItemsSchema = new schema({
   name: { type: String, required: true, minLength: 3, maxLength: 100 },
   quantity: { type: Number, required: true },
   price: { type: Number, required: true },
+  hsn: {type:String, required:true},
   childOf: { type: schema.Types.ObjectId, ref: "ChildCategory" },
 });
 
